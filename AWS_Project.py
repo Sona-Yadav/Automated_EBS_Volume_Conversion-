6dfae87a9373700e2609f906414d6df9ae78c7e8
Sona <127790511+Sona-Yadav@users.noreply.github.com>
2023-10-05 23:36:11 +0530

    Add files via upload

---
+++ AWS_Project.py
@@ -0,0 +1,16 @@
+import json
+import boto3
+def extractVloumeId_from_arn(volume_arn):
+    # Split the arn using the colon(:) separator
+    arn_parts= volume_arn.split(':')
+    volume_id =arn_parts[-1].split('/')[-1]
+    return volume_id
+def lambda_handler(event, context):
+    volume_arn=event['resources'][0]
+    volume_id=extractVloumeId_from_arn(volume_arn)
+   
+    ec2_client=boto3.client('ec2')
+    response = ec2_client.modify_volume(
+        VolumeId=volume_id,
+        VolumeType='gp3',
+    )
